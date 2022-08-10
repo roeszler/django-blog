0a2d555c151088db336e8080ba6ee99ed4791f1d
Stuart Roeszler <stuart@roeszler.com>
2022-08-10 05:58:26 +0000

    add templates .html files and views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    """
+    Built-in class-based generic view from Django to 
+    quickly render our posts
+    """
+    model = Post  # use post as the classes model
+    queryset = Post.objects.filter(status=1).order_by('-created_on')  # status 0 = draft, 1 = published
+    template_name = 'index.html'
+    paginate_by = 6  # separate and limit to display 6 posts, or add page navigation
