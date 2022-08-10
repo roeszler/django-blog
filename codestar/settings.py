a7dd9f5f9979b36f9813213a66801dedf0118c9a
Stuart Roeszler <stuart@roeszler.com>
2022-08-10 11:50:57 +0000

    add email settings and prep for AllAuth authentication

--- codestar/settings.py
+++ codestar/settings.py
@@ -141,3 +141,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
