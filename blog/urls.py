816ae4ee1169144c4424ff4ed2640d241891a94f
Stuart Roeszler <stuart@roeszler.com>
2022-08-10 10:25:16 +0000

    add post_detail.html bootstrap and links to index.html and views.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,15 @@ from . import views
 from django.urls import path
 
 
-# create list
+# create urlpatterns as a list
 urlpatterns = [
     # as we are using views, we need to use as_views on the end of the PostList to render the class as a view
-    path('', views.PostList.as_view(), name='home')  # blank path indicates its our default home page
+    path('', views.PostList.as_view(), name='home'),  # blank path indicates its our default home page
+
+    # First slug is a path converter - converts this text into a slug field, 
+    # It tells Django to match any slug string (ASCII characters or numbers, hyphens, underscores)
+    # Second slug is a keyword name that matches the 'slug' input parameter in the 'get'
+    # function within the PostDetail class within views.py file. 
+    # Creating friendly URLs that consist of our Heroku project URL followed by the slug
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 class PostList(generic.ListView):
     """
     Built-in class-based generic view from Django to 
-    quickly render our posts
+    quickly render the list view of the blog posts
     """
     model = Post  # use post as the classes model
     queryset = Post.objects.filter(status=1).order_by('-created_on')  # status 0 = draft, 1 = published
     template_name = 'index.html'
     paginate_by = 6  # separate and limit to display 6 posts, or add page navigation
+
+
+# Class-based views are different. Instead of using an if statement to check the request method,  
+# we simply create class methods called get or post, or any other HTTP verb.
+class PostDetail(View):
+    """
+    Creates the detail view of a particular blog post.
+    """
+
+    def get(self, request, slug, *args, **kwargs):  # define the function and inputs
+        queryset = Post.objects.filter(status=1)  # only active posts
+        post = get_object_or_404(queryset, slug=slug)  # get object or alternately 404
+        comments = post.comments.filter(approved=True).order_by('created_on')  # to get any comments that are attached to the post
+        liked = False  # default liked status
+        if post.likes.filter(id=self.request.user.id).exists(): # to see if the logged in user has liked this post or not
+            liked = True
+        
+        # Now to send all of this above information to our render method:
+        return render(
+            request,  # send request
+            'post_detail.html',  # supply template we require
+            # dictionary to supply context:
+            {
+                'post': post,
+                'comments': comments,
+                'liked': liked
+            },
+        )
